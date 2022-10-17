177a8d6b858f27aeac3416926772cb189a9772ab
최희원 <jeeen1@naver.com>
2022-10-17 22:37:18 +0900

    feat: mainpage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
 import { global } from "../global";
+import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,41 @@ import styled from "@emotion/styled";
 import { Layout } from "../components/Layout";
 import ShareButton from "../components/ShareButton";
 
+const Laptop = styled.div`
+  width: 720px;
+  height: 450px;
+  background: url("/assets/laptop-filled.png");
+  background-size: cover;
+  background-position: center;
+  border: 1px solid #a5a6ac;
+  border-radius: 30px;
+`;
+
+const DecoButton = styled.button`
+  width: 320px;
+  height: 64px;
+  font-size: 2rem;
+  font-weight: 500;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid white;
+
+  &:hover {
+    background: white;
+  }
+`;
+
 const Home: NextPage = () => {
   return (
     <Layout>
-      <Title>소프트웨어공학</Title>
-      <ShareButton />
+      <main className="bg-[url('/assets/bg-image.png')] bg-center bg-cover">
+        <section className="z-0 flex flex-col gap-16 items-center justify-center w-full h-screen pt-32">
+          <Laptop className="z-50 w-[320px] h-[200px] lg:w-[32vw] lg:h-[20vw] shrink-0" />
+          <DecoButton>DECORATE!</DecoButton>
+        </section>
+      </main>
     </Layout>
   );
 };
